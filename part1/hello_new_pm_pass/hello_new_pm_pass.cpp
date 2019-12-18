8c39a77c13d6afe35b47cd41839ddc582df21a0d
zhouxia <zhouxia@zhouxia.com>
2019-12-18 19:25:37 +0800

    add comments for anonymous function

--- part1/hello_new_pm_pass/hello_new_pm_pass.cpp
+++ part1/hello_new_pm_pass/hello_new_pm_pass.cpp
@@ -5,14 +5,13 @@
 using namespace llvm;
 
 namespace {
-struct HelloNewPMPass : public PassInfoMixin<HelloNewPMPass> {
-  PreservedAnalyses run(Function &F,
-                        FunctionAnalysisManager &FAM) {
-    if(F.hasName())
-      errs() << "Hello " << F.getName() << "\n";
-    return PreservedAnalyses::all();
-  }
-};
+  struct HelloNewPMPass : public PassInfoMixin<HelloNewPMPass> {
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+      if(F.hasName())
+        errs() << "Hello " << F.getName() << "\n";
+      return PreservedAnalyses::all();
+    }
+  };
 } // end anonymous namespace
 
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
@@ -23,7 +22,7 @@ llvmGetPassPluginInfo() {
       PB.registerPipelineParsingCallback(
         [](StringRef Name, FunctionPassManager &FPM,
            ArrayRef<PassBuilder::PipelineElement>) {
-          if(Name == "hello-new-pm-pass"){
+          if(Name == "hello-new-pm-pass"){    //当命令行的参数中有"hello-new-pm-pass"时，就会添加HelloNewPass
             FPM.addPass(HelloNewPMPass());
             return true;
           }
